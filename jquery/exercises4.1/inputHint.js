940317eeb1383576afdb390a937f1f9ab1c483f5
Nishant <nishant@Nishant-PC.(none)>
2012-07-26 18:19:12 +0530

    sub

--- jquery/exercises4.1/inputHint.js
+++ jquery/exercises4.1/inputHint.js
@@ -13,14 +13,14 @@ $(document).ready( function() {
     console.log('label is removed', $input_search.prev('label[for="q"]').detach());
 
     $input_search.focus(function() {
-        console.log('hint is removed');
+        console.log('hint is added');
         $(this).attr('value','');
         $(this).removeClass('hint')
     });
 
     $input_search.blur(function() {
         if( $(this).attr('value') != null && $(this).attr('value').length === 0 ) {
-        console.log('hint is added');
+        console.log('hint is removed');
         $(this).attr('value','hint');
         $(this).addClass('hint') }
     });

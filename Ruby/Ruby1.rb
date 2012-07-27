f2cb5411846c66d54877206981b81b9ce833d106
Nishant <nishant@Nishant-PC.(none)>
2012-07-27 12:32:35 +0530

    ruby

---
+++ Ruby/Ruby1.rb
@@ -0,0 +1,25 @@
+
+def count_occurance
+
+    puts "Enter a string"
+
+    answer = gets.chomp
+
+    occurance = Hash.new
+
+    puts answer
+
+     answer.each_char do |char|
+       if occurance.has_key?(char)
+           occurance[char] += 1
+       else
+           occurance[char] = 1
+       end
+   end
+
+   occurance.each do |key, value|
+	   puts "#{key} - #{value}"
+   end
+end
+
+count_occurance

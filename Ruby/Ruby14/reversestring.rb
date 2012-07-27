7dc6ea7dabecaddf32fe04ac026d74f035572609
Nishant <nishant@Nishant-PC.(none)>
2012-07-27 19:18:51 +0530

    ruby

---
+++ Ruby/Ruby14/reversestring.rb
@@ -0,0 +1,10 @@
+class ReverseString
+	
+	def initialize(str)
+		@str = str
+	end
+
+	def to_s
+		@str.split(" ").reverse.join(" ")
+	end
+end
---
+++ Ruby/Ruby14/ruby14.rb
@@ -0,0 +1,13 @@
+require_relative 'reversestring'
+
+
+def to_reverse_string
+    puts "Enter the String to reverse"
+
+    reverse_str = ReverseString.new(gets.chomp)
+
+    puts reverse_str
+end
+
+
+to_reverse_string
